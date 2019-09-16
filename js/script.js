38a1c1d058eec2b0377dc4435c9f758401d0f161
Misiek40k <comanderrr@gmail.com>
2019-09-16 20:54:05 +0200

    Add titleClickHandler function and loops for deleting active class

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const titleClickHandler = (event) => {
+
+    const activeLinks = document.querySelectorAll('.titles a.active');
+    const activeArticles = document.querySelectorAll('.posts .post.active');
+
+    /* remove class 'active' from all article links  */
+
+    for (let activeLink of activeLinks) {
+        activeLink.classList.remove('active');
+    }
+
+    /* add class 'active' to the clicked link */
+
+    /* remove class 'active' from all articles */
+
+    for (let activeArticle of activeArticles) {
+        activeArticle.classList.remove('active');
+    }
+
+    /* get 'href' attribute from the clicked link */
+
+    /* find the correct article using the selector (value of 'href' attribute) */
+
+    /* add class 'active' to the correct article */
+}
+
+const links = document.querySelectorAll('.titles a');
+
+for (let link of links) {
+    link.addEventListener('click', titleClickHandler);
+}
