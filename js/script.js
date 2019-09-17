ff9b4ba143aa426909c1ecf86ac6fd77e704ea42
Misiek40k <comanderrr@gmail.com>
2019-09-17 19:27:25 +0200

    Delete comments in titleClickHandler

--- js/script.js
+++ js/script.js
@@ -8,26 +8,16 @@ const titleClickHandler = (event) => {
     const activeArticles = document.querySelectorAll('.posts .post.active');
     const articleSelector = clickedElement.getAttribute('href');
     const targetArticle = document.querySelector(articleSelector);
-  
-    /* remove class 'active' from all article links  */
 
     for (let activeLink of activeLinks) {
         activeLink.classList.remove('active');
     }
 
-    /* add class 'active' to the clicked link */
-
     clickedElement.classList.add('active');
-
-    /* remove class 'active' from all articles */
 
     for (let activeArticle of activeArticles) {
         activeArticle.classList.remove('active');
     }
-
-    /* get 'href' attribute from the clicked link */
-    /* find the correct article using the selector (value of 'href' attribute) */
-    /* add class 'active' to the correct article */
 
     targetArticle.classList.add('active');
 }
