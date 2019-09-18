95f6867f47083a33030be529d3c1fba634e6cd82
Misiek40k <comanderrr@gmail.com>
2019-09-18 09:05:34 +0200

    Add opt consts and start generateTitleLinks template

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const optArticleSelector = '.post';
+const optTitleSelector = '.post-title';
+const optTitleListSelector = '.titles';
+
 const titleClickHandler = (event) => {
 
     event.preventDefault();
@@ -27,3 +31,23 @@ const links = document.querySelectorAll('.titles a');
 for (let link of links) {
     link.addEventListener('click', titleClickHandler);
 }
+
+const generateTitleLinks = () => {
+
+    /* remove contents of titleList */
+
+    /* for each article */
+
+    /* get the article id */
+
+    /* find the title element */
+
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+
+    /* insert link into titleList */
+
+}
+
+generateTitleLinks();
